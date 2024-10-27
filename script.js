f32beb25c7ffdd279700ce9c50ed6d15634d61ef
Nguyễn Thanh Hào <67219502+nguyenthanhhao18012000@users.noreply.github.com>
2024-10-27 17:49:09 +0700

    pull code

---
+++ script.js
@@ -0,0 +1,14 @@
+// Event listener on the form submit
+document.querySelector("#email-form").addEventListener("submit", function(event){
+    event.preventDefault()
+
+    const emailForm = document.querySelector("#email")
+    const email = emailForm.value
+
+    console.log(email)
+
+    // Make RudderStack Identify Call
+    rudderanalytics.idenitfy(email, { email: email })
+
+    emailForm.value = ""
+} )
